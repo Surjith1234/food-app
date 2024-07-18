6d5bed99caa34d64c5d13992a2945041c1582e33
Surjith S <153822262+Surjith1234@users.noreply.github.com>
2024-07-18 10:58:33 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -13,16 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/',(req, res) => {
   res.send('Hello World');
 });
-const connect=async()=>{
-    try {
-        await mongoose.connect('mongodb+srv://surjith:surjith2003@cluster0.pw1ezze.mongodb.net/food?retryWrites=true&w=majority&appName=Cluster0')
-        console.log("connected");
-    }
-    catch(err)
-    {
-        throw err;
-    }
-}
+// const connect=async()=>{
+//     try {
+//         await mongoose.connect('mongodb+srv://surjith:surjith2003@cluster0.pw1ezze.mongodb.net/food?retryWrites=true&w=majority&appName=Cluster0')
+//         console.log("connected");
+//     }
+//     catch(err)
+//     {
+//         throw err;
+//     }
+// }
 app.use(cors(
     {
         origin:["https://deploy-mern-1whq.versal.app"],
@@ -36,6 +36,6 @@ app.use('/api/food',require('./routes/food'))
 app.use('/api/order',require('./routes/order'))
 app.use(express.json({limit:"3mb"}))
 app.listen(5000,()=>{
-    connect();
+   // connect();
     console.log("port is running on 5000");
 })
